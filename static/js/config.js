255e8c4a49ba47c0f904017850081b990156a418
HuangJyunYu <yillkid@gmail.com>
2024-05-20 19:57:46 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -3,7 +3,7 @@ HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
 HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
 
 // EID
-HOST_URL_EID = "https://eid.townway.com.tw";
+HOST_URL_EID = "https://eid-stu.townway.com.tw";
 
 // Site hoster
 SITE_HOSTERS = ["Real.ability2022@gmail.com"]
